cd864d97da63153c8fa71f69be4d7777e6e4371b
axl.dm82 <axl.dm82@gmail.com>
2023-12-13 16:26:36 +0100

    01

--- src/main/java/org/acme/geometry/Coordinate.java
+++ src/main/java/org/acme/geometry/Coordinate.java
@@ -1,5 +1,38 @@
 package org.acme.geometry;
 
 public class Coordinate {
+    private final double x;
+    private final double y;
 
+    // Constructeur par défaut initialisant une coordonnée vide
+    public Coordinate() {
+        this(Double.NaN, Double.NaN);
+    }
+
+    // Constructeur avec valeurs spécifiées
+    public Coordinate(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    // Méthode pour tester si une coordonnée est vide
+    public boolean isEmpty() {
+        return Double.isNaN(x) && Double.isNaN(y);
+    }
+
+    // Méthode pour obtenir la valeur de x
+    public double getX() {
+        return x;
+    }
+
+    // Méthode pour obtenir la valeur de y
+    public double getY() {
+        return y;
+    }
+
+    // Méthode pour représenter la coordonnée sous forme de chaîne au format JSON
+    @Override
+    public String toString() {
+        return "[" + x + "," + y + "]";
+    }
 }
--- src/test/java/org/acme/geometry/CoordinateTest.java
+++ src/test/java/org/acme/geometry/CoordinateTest.java
@@ -1,18 +1,23 @@
 package org.acme.geometry;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import org.junit.Assert;
 import org.junit.Test;
 
 public class CoordinateTest {
 
-	public static final double EPSILON = 1.0e-15;
+    public static final double EPSILON = 1.0e-15;
 
-	@Test
-	public void testConstructorXY(){
-		// TODO
-		//Coordinate c = new Coordinate(3.0,4.0);
-		//Assert.assertEquals(3.0, c.getX(), EPSILON);
-		//Assert.assertEquals(4.0, c.getY(), EPSILON);
-	}
+    @Test
+    public void testCoordinateXY() {
+        Coordinate c = new Coordinate(3.0, 4.0);
+        assertEquals(3.0, c.getX(), EPSILON);
+        assertEquals(4.0, c.getY(), EPSILON);
+        assertFalse(c.isEmpty());
+        assertEquals("[3.0,4.0]", c.toString());
+		System.out.println(c.getY());
+    }
 
 }
