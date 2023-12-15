87a8e51c14a333298e458c8762f3a5b2b5299dee
axl.dm82 <axl.dm82@gmail.com>
2023-12-15 13:48:33 +0100

    test 11

---
+++ src/test/java/org/acme/geometry/AsTextTest.java
@@ -0,0 +1,42 @@
+package org.acme.geometry;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AsTextTest {
+    private static final double EPSILON = 1e-15;
+
+    @Test
+    public void testasTextPoint() {
+        Point point = new Point(new Coordinate(3.0, 4.0));
+        assertEquals("POINT(3.0 4.0)",point.asText());
+    }
+
+    @Test
+    public void testasTextEmptyPoint() {
+        Geometry emptyPoint = new Point();
+        assertEquals("POINT EMPTY", emptyPoint.asText());
+    }
+
+    @Test
+    public void testasTextLineString() {
+        List<Point> points = Arrays.asList(
+                new Point(new Coordinate(0.0, 0.0)),
+                new Point(new Coordinate(1.0, 1.0)),
+                new Point(new Coordinate(5.0, 5.0))
+        );
+        Geometry lineString = new LineString(points);
+        assertEquals("LINESTRING(0.0 0.0,1.0 1.0,5.0 5.0)", lineString.asText());
+    }
+
+    @Test
+    public void testasTextEmptyLineString() {
+        Geometry emptyLineString = new LineString();
+        assertEquals("LINESTRING EMPTY", emptyLineString.asText());
+    }
+}
